e974e5225e9f429273dff6bef5ccc6abdeb2fc91
ntran0429 <ntran0429@gmail.com>
2024-10-26 23:44:12 -0500

    created window painting black and closing properly

--- main.py
+++ main.py
@@ -9,5 +9,14 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(color= (0, 0, 0))
+        pygame.display.flip()
+
+
 if __name__ == "__main__":
     main()
