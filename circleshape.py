f56bcc54a45ee9fcc3e23fa34d67b1240e2b5dcf
ntran0429 <ntran0429@gmail.com>
2024-10-27 00:41:46 -0500

    initialized player object; a black screen with a white triangle in the middle

---
+++ circleshape.py
@@ -0,0 +1,23 @@
+import pygame
+
+# Base class for game objects
+# to represent objects in our game that are treated as circles (even if they aren't)
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        # CircleShape extends the Sprite class to also store a position, velocity, and radius
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +18,7 @@ def main():
                 return
 
         screen.fill(color= (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         
         # limit the framerate to 60 FPS
